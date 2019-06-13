ec1c3eb0e342133ecc88fb2cde7ed96d60cff19c
GOOTT-06 <GOOTT-06@GOOTT-06-PC>
2019-06-13 12:53:51 +0900

    프로젝트 수정 scm-->sdj by sdj

--- sdj-project/src/testMain.java
+++ sdj-project/src/testMain.java
@@ -3,7 +3,7 @@ public class testMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, Git SCM");
+		System.out.println("Hello, Git SDJ");
 
 	}
 
