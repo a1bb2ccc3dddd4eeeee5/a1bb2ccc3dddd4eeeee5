8c8ade6bbf5c902b982146977a768ff20aeb1798
GOOTT-06 <GOOTT-06@GOOTT-06-PC>
2019-06-13 12:59:44 +0900

    출력한줄추가

--- sdj-project/src/testMain.java
+++ sdj-project/src/testMain.java
@@ -4,6 +4,7 @@ public class testMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, Git SDJ");
+		System.out.println("Hello, Git a1bb2ccc3dddd4eeeee5");
 
 	}
 
